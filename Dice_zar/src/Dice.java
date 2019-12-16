6ea51c8249db3b21781e9e5df269bf2e10c33c8f
cristianabote <cristianamihaelaplesa@gmail.com>
2019-12-16 23:12:56 +0200

    versiune 1

---
+++ Dice_zar/src/Dice.java
@@ -0,0 +1,16 @@
+public class Dice {
+    public static void main(String [] args ){
+        int a[]={1,2,6,6}, i, n=4, ok=1, g=0;
+        for(i=0; i<n;i++) {
+            if (a[n - 1] == 6)
+                ok = -1;
+            else if (a[i] != 6)
+                g += 1;
+        }
+        if(ok==1)
+            System.out.println(g + "number of girls");
+        else
+        if(ok==-1)
+            System.out.println(ok + " secventa invalida");
+    }
+}
